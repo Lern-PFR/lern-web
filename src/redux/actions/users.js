a2251cfc2485e09e1a04bce9b1488100cb82e9ec
Hynel <timothee.simonfranza@viacesi.fr>
2020-06-03 11:44:28 +0200

    Setup redux and unit testing, as well as GitHub actions

---
+++ src/redux/actions/users.js
@@ -0,0 +1,4 @@
+export const updateUsersAction = ({ users }) => ({
+    type: 'USERS_UPDATE_ALL',
+    payload: { users },    
+});
---
+++ tests/redux/reducers/users.js
@@ -0,0 +1,31 @@
+import usersReducer from '../../../src/redux/reducers/users';
+
+describe('users reducer', () => {
+
+    describe('initial state', () => {
+        it('should return initial user state', () => {
+            const action = { type: 'dummy_action' };
+            const initialState = {
+                init: false,
+                items: [],
+            };
+
+            expect(usersReducer(undefined, action)).toEqual(initialState);
+        });
+
+        it('should return the inputted users list with init value set to true', () => {
+            const users = [
+                { id: 0, username: 'johndoe' },
+                { id: 1, username: 'janedoe' }
+            ];
+          const action = { type: 'USERS_UPDATE_ALL', payload: { users } };
+          
+          const expectedState = { 
+              init: true,
+              items: users
+           };
+      
+          expect(usersReducer(undefined, action)).toEqual(expectedState);
+        });
+    });
+});
