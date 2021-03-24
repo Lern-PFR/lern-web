828d49125d290759a04dd18475e1607b377a42a5
Christopher Walker <christophersvt3g@gmail.com>
2021-03-24 18:14:19 +0100

    updated elevations theme

--- src/theme/elevations.js
+++ src/theme/elevations.js
@@ -1,16 +1,24 @@
+const commonElevation = {
+	margin: '10px',
+};
+
 const elevationFlat = {
+	...commonElevation,
 	boxShadow: 'none',
 };
 
 const elevationFirst = {
+	...commonElevation,
 	boxShadow: '5px 5px 5px',
 };
 
 const elevationSecond = {
+	...commonElevation,
 	boxShadow: '5px 10px 10px',
 };
 
 const elevationThird = {
+	...commonElevation,
 	boxShadow: '5px 15px 15px',
 };
 
