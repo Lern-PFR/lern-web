4010c47c9e51410a321cc02de597de9ce92104e7
Timothee Simon-Franza <timothee.simonfranza@viacesi.fr>
2021-03-28 16:37:44 +0200

    Implemented subject reducer along with its unit tests.

---
+++ src/redux/reducers/subjects.js
@@ -0,0 +1,53 @@
+import { ActionTypes } from 'redux/actions/subjects';
+
+const initialState = {
+	isLoading: false,
+	items: [],
+	totalCount: 0,
+};
+
+export default (state = initialState, { type, payload }) => {
+	switch (type) {
+		case ActionTypes.FETCH_SUBJECT_REQUEST:
+		case ActionTypes.FETCH_SUBJECT_LIST_REQUEST:
+		case ActionTypes.CREATE_SUBJECT_REQUEST:
+		case ActionTypes.UPDATE_SUBJECT_REQUEST:
+		case ActionTypes.DELETE_SUBJECT_REQUEST:
+			return {
+				...state,
+				isLoading: true,
+			};
+		case ActionTypes.FETCH_SUBJECT_FAILURE:
+		case ActionTypes.FETCH_SUBJECT_LIST_FAILURE:
+		case ActionTypes.CREATE_SUBJECT_FAILURE:
+		case ActionTypes.UPDATE_SUBJECT_FAILURE:
+		case ActionTypes.DELETE_SUBJECT_FAILURE:
+			return {
+				...state,
+				isLoading: false,
+			};
+		case ActionTypes.CREATE_SUBJECT_SUCCESS:
+		case ActionTypes.UPDATE_SUBJECT_SUCCESS:
+		case ActionTypes.DELETE_SUBJECT_SUCCESS:
+			return {
+				...state,
+				isLoading: false,
+			};
+		case ActionTypes.FETCH_SUBJECT_SUCCESS:
+			return {
+				...state,
+				isLoading: false,
+				items: [payload.subject],
+				totalCount: 1,
+			};
+		case ActionTypes.FETCH_SUBJECT_LIST_SUCCESS:
+			return {
+				...state,
+				isLoading: false,
+				items: [...payload.subjects],
+				totalCount: payload.totalCount,
+			};
+		default:
+			return state;
+	}
+};
