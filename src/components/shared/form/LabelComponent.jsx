cd2555927abb7d3e7deb2efdbe9043f1c77e7404
Christopher Walker <christophersvt3g@gmail.com>
2021-05-25 11:58:40 +0200

    Made requested changes

--- src/components/shared/form/LabelComponent.jsx
+++ src/components/shared/form/LabelComponent.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { labelStyle } from 'theme/formStyles';
 import DynamicTextComponent from 'components/shared/typography/DynamicTextComponent';
@@ -13,30 +14,34 @@ import { canon, greatPrimer, doublePica, bodyCopy, brevier, longPrimer, minion, 
  * @param {string}	[typography]	: The text style of the label text.
  * @param {string}	children		: The test to be displayed in the label.
  */
-const LabelComponent = ({ children, typography, forId, ...otherProps }) => (
-	<DynamicTextComponent
-		{...(() => {
-			switch (typography) {
-				case 'canon':		return canon;
-				case 'trafalgar':	return trafalgar;
-				case 'paragon':		return paragon;
-				case 'greatprimer':	return greatPrimer;
-				case 'doublepica':	return doublePica;
-				case 'bodycopy':	return bodyCopy;
-				case 'pica':		return pica;
-				case 'longprimer':	return longPrimer;
-				case 'minion':		return minion;
-				default:			return brevier;
-			}
-		})()}
-		tag="label"
-		{...labelStyle}
-		htmlFor={forId}
-		{...otherProps}
-	>
-		{children}
-	</DynamicTextComponent>
-);
+const LabelComponent = ({ children, typography, forId, ...otherProps }) => {
+	const textStyle = useMemo(() => {
+		switch (typography.toLowerCase()) {
+			case 'canon':		return canon;
+			case 'trafalgar':	return trafalgar;
+			case 'paragon':		return paragon;
+			case 'greatprimer':	return greatPrimer;
+			case 'doublepica':	return doublePica;
+			case 'bodycopy':	return bodyCopy;
+			case 'pica':		return pica;
+			case 'longprimer':	return longPrimer;
+			case 'minion':		return minion;
+			default:			return brevier;
+		}
+	}, [typography]);
+
+	return (
+		<DynamicTextComponent
+			tag="label"
+			htmlFor={forId}
+			{...textStyle}
+			{...labelStyle}
+			{...otherProps}
+		>
+			{children}
+		</DynamicTextComponent>
+	);
+};
 
 LabelComponent.propTypes = {
 	children: PropTypes.string.isRequired,
