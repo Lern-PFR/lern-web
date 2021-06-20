562105839d1fbb3b59d13c9796a43eb975c5c965
Christopher Walker <christophersvt3g@gmail.com>
2021-06-20 18:09:24 +0200

    Mocked missing fetch methods in Homepage unit test

--- tests/pages/home/Homepage.test.js
+++ tests/pages/home/Homepage.test.js
@@ -3,7 +3,9 @@ import { Provider } from 'react-redux';
 import { StaticRouter } from 'react-router';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import { baseUrl } from 'lib/shared/http';
 import { Homepage } from 'pages/home';
+import fetchMock from 'fetch-mock';
 
 const mockStore = configureMockStore([thunk]);
 
@@ -11,6 +13,7 @@ describe('Homepage', () => {
 	afterEach(() => {
 		jest.restoreAllMocks();
 		jest.resetAllMocks();
+		fetchMock.restore();
 	});
 
 	it('should match previous snapshot for anonymous user homepage', () => {
@@ -26,6 +29,50 @@ describe('Homepage', () => {
 				</StaticRouter>
 			</Provider>
 		);
+
+		const subjectList = {
+			status: 200,
+			body: {
+				all: [],
+				mine: [],
+				active: [],
+				available: [
+					{
+						id: 'abcd',
+						title: 'dummy module',
+						description: 'dummy description',
+					},
+				],
+			},
+			headers: { 'content-type': 'application/json' },
+		};
+
+		const progressionList = {
+			status: 200,
+			body: {
+				subject: {
+					id: 'abcd',
+					title: 'dummy module',
+					description: 'dummy description',
+				},
+				user: {
+					id: 'abcd',
+					nickname: 'dummy module',
+					firstname: 'dummy description',
+					lastname: 'dummy lastname',
+				},
+				concept: {
+					id: 'abcd',
+					title: 'dummy module',
+					description: 'dummy description',
+				},
+			},
+			headers: { 'content-type': 'application/json' },
+		};
+
+		fetchMock.get(`${baseUrl}/api/progression`, progressionList);
+		fetchMock.get(`${baseUrl}/api/subjects`, subjectList);
+
 		const wrapper = mount(sut);
 
 		expect(wrapper).toMatchSnapshot();
@@ -48,6 +95,50 @@ describe('Homepage', () => {
 				</StaticRouter>
 			</Provider>
 		);
+
+		const subjectList = {
+			status: 200,
+			body: {
+				all: [],
+				mine: [],
+				active: [],
+				available: [
+					{
+						id: 'abcd',
+						title: 'dummy module',
+						description: 'dummy description',
+					},
+				],
+			},
+			headers: { 'content-type': 'application/json' },
+		};
+
+		const progressionList = {
+			status: 200,
+			body: {
+				subject: {
+					id: 'abcd',
+					title: 'dummy module',
+					description: 'dummy description',
+				},
+				user: {
+					id: 'abcd',
+					nickname: 'dummy module',
+					firstname: 'dummy description',
+					lastname: 'dummy lastname',
+				},
+				concept: {
+					id: 'abcd',
+					title: 'dummy module',
+					description: 'dummy description',
+				},
+			},
+			headers: { 'content-type': 'application/json' },
+		};
+
+		fetchMock.get(`${baseUrl}/api/progression`, progressionList);
+		fetchMock.get(`${baseUrl}/api/subjects`, subjectList);
+
 		const wrapper = mount(sut);
 
 		expect(wrapper).toMatchSnapshot();
